033794693dfd4ba215e88031133ce99c6bc53225
Ramanuj <ramanuj0705@gmail.com>
2023-12-26 00:26:02 +0530

    episode 1 code

---
+++ App.js
@@ -0,0 +1,29 @@
+const parent = React.createElement(
+    'div', 
+    {id: 'parent'},
+    [
+        React.createElement(
+            'div',
+            {id: 'child'},
+            [
+                React.createElement('h1', {}, 'I am an h1 tag of child'),
+                React.createElement('h2', {}, 'I am an h2 tag in child class')
+            ]
+        ), 
+
+        React.createElement(
+            'div',
+            {id: 'child2'},
+            [
+                React.createElement('h1', {}, 'I am an h1 tag of child2'),
+                React.createElement('h2', {}, 'I am an h2 tag in child2 class')
+            ]
+        )
+    ]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(parent);
