54be3467fa3f1e4b32748d09aa88d569c14f78de
Ramanuj <ramanuj0705@gmail.com>
2023-12-27 03:05:09 +0530

    episode-2 code of Namaste React

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement(
-    'div', 
-    {id: 'parent'},
+    'div',
+    { id: 'parent' },
     [
         React.createElement(
             'div',
-            {id: 'child'},
+            { id: 'child' },
             [
                 React.createElement('h1', {}, 'I am an h1 tag of child'),
                 React.createElement('h2', {}, 'I am an h2 tag in child class')
             ]
-        ), 
+        ),
 
         React.createElement(
             'div',
-            {id: 'child2'},
+            { id: 'child2' },
             [
                 React.createElement('h1', {}, 'I am an h1 tag of child2'),
                 React.createElement('h2', {}, 'I am an h2 tag in child2 class')
