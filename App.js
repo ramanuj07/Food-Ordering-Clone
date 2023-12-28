085e7e98a456a66a67817540800f13bbbd7c0cb0
Ramanuj <ramanuj0705@gmail.com>
2023-12-28 23:45:56 +0530

    Building a food-ordering app

--- App.js
+++ App.js
@@ -1,23 +1,91 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 id="heading" tabIndex={4}>
-    My name is Ramanuj Jindal
-  </h1>
-);
+// preparing a food ordering website like swiggy or zomato
+// firstly a design needs to be made for the website
+/** design -
+ * HEADER -
+ *      - LOGO
+ *      - NAV ITEMS
+ * BODY -
+ *      - SEARCH
+ *      - RESTAURANT-CONTAINER
+ *             - RESTAURANT CARDS
+ *                  - Image,
+ *                  - Name, Star Rating, Cuisine, Delivery Time
+ * FOOTER -
+ *      - COPYRIGHT
+ *      - LINKS
+ *      - ADDRESS
+ */
 
-const anyNumber = 10010010;
+const resCard = {
+  backgroundColor: "#f0f0f0",
+};
 
-// Functional component
-const HeadingComponent = () => (
-  <>
-    <h1 className="heading">Heyyy Functional Component beauty</h1>
-    <Title />
-    {anyNumber}
-  </>
-);
+const RestuarantCard = () => {
+  return (
+    <div className="restaurant--card" style={resCard}>
+      <img src="https://media-cdn.tripadvisor.com/media/photo-s/19/1e/1a/3a/pizza-hut.jpg" />
+      <h2>Pizza Hut</h2>
+      <p>5</p>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <input type="search" />
+        <button type="submit">Search</button>
+      </div>
+
+      <div className="restaurant--container">
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+        <RestuarantCard />
+      </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo--container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg"
+        />
+      </div>
+
+      <div className="nav--items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
